437eae61ddcd48749d2ac54b9053e9acd5178044
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-03-29 17:01:39 +0000

    Update main.java

--- main.java
+++ main.java
@@ -149,6 +149,49 @@ public class TicketMachine
      * Update the total collected and
      * reduce the balance to zero.
      */
+    
+    public class TicketMachine
+{
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
+    //The price is given to customers
+    //This field determines whether or not there is a bus around or not 
+    private boolean isAround;
+    
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public TicketMachine(int cost)
+    {
+        price = cost;
+        balance = 0;
+        total = 0;   
+        isAround = true;
+        
+    }
+
+    /**
+     * Return the price of a ticket.
+     */
+    public int getPrice()
+    {
+        return price;
+    }
+
+    /**
+     * Return the amount of money already inserted for the
+     * next ticket.
+     */
+    public int getAmount()
+    {
+        return balance;
+    }
     public void printTicket()
     {
         // Simulate the printing of a ticket.
