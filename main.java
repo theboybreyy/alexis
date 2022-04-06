6a03cd3e6d097b7b750a8d12ba43e41971e27d08
Lex Benjamin <86781764+theboybreyy@users.noreply.github.com>
2022-04-06 15:17:11 +0000

    Update main.java

--- main.java
+++ main.java
@@ -6,7 +6,17 @@
  * It is a naive machine in the sense that it trusts its users
  * to insert enough money before trying to print a ticket.
  * It also assumes that users enter sensible amounts.
+ * /**
+ * TicketMachine models a naive ticket machine that issues
+ * flat-fare tickets.
+ * The price of a ticket is specified via the constructor.
+ * It is a naive machine in the sense that it trusts its users
+ * to insert enough money before trying to print a ticket.
+ * It also assumes that users enter sensible amounts.
  *
+ * @author @theboybreyy
+ * @version 2016.02.29
+ */
  * @author @theboybreyy
  * @version 2016.02.29
  */
